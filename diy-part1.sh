c09d076125481ec00181bd3fdadaaa9b1db706e7
stonegr <eloisasethxdg62@gmail.com>
2025-06-11 14:26:34 +0800

    init

---
+++ diy-part1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# add nf_deaf
+git clone https://github.com/stonegr/nf_deaf_op.git package/kernel/nf-deaf
---
+++ diy-part2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+sed -i 's/LEDE/Stone/g' package/base-files/files/bin/config_generate
